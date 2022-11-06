7e7ac02838d14f0fe271b046bd4baec043c235ba
Rafael Martins <rafaelmartinsja@live.com>
2022-11-06 16:13:44 -0300

    Add SAV_API in C Sharp.

---
+++ SAV_API/SAV_API/Controllers/ViagemController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAV_API.Models;
+
+namespace SAV_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViagemController : ControllerBase
+    {
+        private readonly ViagemDbContext _context;
+
+        public ViagemController(ViagemDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Viagem - LISTA TODOS as Viagems
+        [HttpGet]
+        public IEnumerable<Viagem> GetViagens()
+        {
+            return _context.Viagens;
+        }
+
+        // GET: api/Viagem/id - LISTA Viagens POR ID
+        [HttpGet("{id}")]
+        public IActionResult GetViagemPorId(int id)
+        {
+            Viagem viagem = _context.Viagens.SingleOrDefault(modelo => modelo.id == id);
+            if (viagem == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(viagem);
+        }
+
+        // CRIA uma nova Viagem
+        [HttpPost]
+        public IActionResult CriarViagem(Viagem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+
+
+            _context.Viagens.Add(item);
+            _context.SaveChanges();
+            return new ObjectResult(item);
+
+        }
+
+        // ATUALIZA UMa Viagem EXISTENTE
+        [HttpPut("{id}")]
+        public IActionResult AtualizaViagem(int id, Viagem item)
+        {
+            if (id != item.id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+
+
+
+            return new NoContentResult();
+        }
+
+        // APAGA UMa Viagem POR ID
+        [HttpDelete("{id}")]
+        public IActionResult DeletaViagem(int id)
+        {
+            var viagem = _context.Viagens.SingleOrDefault(m => m.id == id);
+
+
+
+            if (viagem == null)
+            {
+                return BadRequest();
+            }
+
+
+
+            _context.Viagens.Remove(viagem);
+            _context.SaveChanges();
+            return Ok(viagem);
+        }
+    }
+}
---
+++ SAV_API/SAV_API/Models/ViagemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SAV_API.Models
+{
+    public class ViagemDbContext : DbContext
+    {
+        public ViagemDbContext(DbContextOptions<ViagemDbContext> options)
+          : base(options)
+        { }
+
+
+
+        public DbSet<Viagem> Viagens { get; set; }
+    }
+}
